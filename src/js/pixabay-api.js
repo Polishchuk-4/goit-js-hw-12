fedc1a65e4c4c3dc37aae865dffda1871bdec295
Denys Polishchuk <polischyk0000@gmail.com>
2024-05-28 15:31:52 +0300

    create axios.get

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,24 @@
-export default function fetchURL(userInput) {
+import axios from 'axios';
+export default async function fetchURL(userInput) {
   var API_KEY = '44071670-df551d8a467c5c77b993c0d31';
-
-  var URL = `https://pixabay.com/api/?key=${API_KEY}&q=${userInput}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-  return fetch(URL, {
-    method: 'GET',
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(values => {
-      return values;
-    })
-    .catch(error => console.log('--Error--:', error));
+  const response = await axios.get(`https://pixabay.com/api/`, {
+    params: {
+      key: API_KEY,
+      q: userInput,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+    },
+  });
+  return response.data;
 }
+// const searchParams = new URLSearchParams({
+//   key: API_KEY,
+//   q: userInput,
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: true,
+//   per_page: 15,
+// });
+// const response = await axios.get(`https://pixabay.com/api/?${searchParams}.`);
